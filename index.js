af5201e95a1ba6544c3f5727ea08e75b7406c59b
Alex Ross <alxrg@me.com>
2023-06-24 20:33:27 -0500

    completed 2 challenges and working

--- index.js
+++ index.js
@@ -5,13 +5,29 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// Define the users array
+const users = [];
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+  io.emit('user entered');
+  socket.on('sendNickname', (username) => {
+    socket.username = username; // assign the username
+    users.push(socket.username);
+  });
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    io.emit('chat message', { message: msg, username: socket.username });
+  });
+  socket.on('disconnect', () => {
+    io.emit('user left');
+    // Remove the disconnected user from the users array
+    const index = users.indexOf(socket.username);
+    if (index !== -1) {
+      users.splice(index, 1);
+    }
   });
 });
 
